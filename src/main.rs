0d8fcfd91deded680e988a8762af05422fe5173f
newnakashima <newnakashima2014@gmail.com>
2022-07-04 13:42:48 +0900

    feat: 参画したプロジェクト一覧を出力

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,17 @@ fn main() {
     let personal = personal_block(&docs[0]["personal"]);
     let certificates = certificates_block( &docs[0]["certificates"]);
     let self_introduction = self_introduction_block(&docs[0]["self_introduction"]);
+    let projects = projects_block(&docs[0]["projects"]);
 
     let sheet = format!("
 # スキルシート
 
 {personal}
 {certificates}
-{self_introduction}");
+{self_introduction}
+### 参画したプロジェクト一覧
+{projects}
+");
 
     println!("{}", sheet);
 }
@@ -42,19 +46,19 @@ fn certificates_block(certificates: &Yaml) -> String {
 
     return format!("
 ## 資格
-| 名前 | 生年月日 | 備考 |
+| 名前 | 取得年月 | 備考 |
 | --- | --- | --- |
 | {name} | {certified_at} | {comment} |
 ")
 }
 
-fn name_description_list(available: &Vec<Yaml>) -> String {
-    let mut available_list: Vec<String> = vec![];
+fn name_description_list(list: &Vec<Yaml>) -> String {
+    let mut string_list: Vec<String> = vec![];
 
-    for x in available {
+    for x in list {
         let name = x["name"].as_str().unwrap();
         if x["descriptions"].as_vec().is_none() {
-            available_list.push(format!("
+            string_list.push(format!("
 - {name}
 "
             ));
@@ -71,26 +75,94 @@ fn name_description_list(available: &Vec<Yaml>) -> String {
         }
         let description = description_list.join("\n");
 
-        available_list.push(format!("
+        string_list.push(format!("
 - {name}
 {description}
 "
         ));
     }
 
-    return available_list.join("");
+    return string_list.join("");
 }
 
 fn self_introduction_block(self_introduction: &Yaml) -> String {
-    let available_vec = self_introduction["available"].as_vec().unwrap();
-    let available = name_description_list(available_vec);
+    let available_vec =
+        self_introduction["available"].as_vec().unwrap();
+    let available =
+        name_description_list(available_vec);
 
-    let languages_use_frequently_vec = self_introduction["languages_use_frequently"].as_vec().unwrap();
-    let languages_use_frequently = name_description_list(languages_use_frequently_vec);
+    let languages_use_frequently_vec = 
+        self_introduction["languages_use_frequently"].as_vec().unwrap();
+    let languages_use_frequently = 
+        name_description_list(languages_use_frequently_vec);
+
+    let languages_used_ever_vec = 
+        self_introduction["languages_used_ever"].as_vec().unwrap();
+    let languages_used_ever =
+        name_description_list(languages_used_ever_vec);
+
+    let middleware_use_frequently_vec =
+        self_introduction["middleware_use_frequently"].as_vec().unwrap();
+    let middleware_use_frequently =
+        name_description_list(middleware_use_frequently_vec);
+
+    let middleware_used_ever_vec =
+        self_introduction["middleware_used_ever"].as_vec().unwrap();
+    let middleware_used_ever = 
+        name_description_list(middleware_used_ever_vec);
+    
+    let virtual_environment_use_frequently_vec =
+        self_introduction["virtual_environment_use_frequently"].as_vec().unwrap();
+    let virtual_environment_use_frequently =
+        name_description_list(virtual_environment_use_frequently_vec);
+    
+    let cloud_platform_use_frequently_vec =
+        self_introduction["cloud_platform_use_frequently"].as_vec().unwrap();
+    let cloud_platform_use_frequently =
+        name_description_list(cloud_platform_use_frequently_vec);
 
     return format!("
 ## 自己PR
-### 対応可能な言語
+### 対応可能な業務
 {available}
-{languages_use_frequently}")
+### 得意な言語
+{languages_use_frequently}
+### 業務で使用したことのある言語
+{languages_used_ever}
+### よく使用するミドルウェア
+{middleware_use_frequently}
+### 業務で使用したことのあるミドルウェア
+{middleware_used_ever}
+### よく使用する仮想環境
+{virtual_environment_use_frequently}
+### 得意なクラウドプラットフォーム
+{cloud_platform_use_frequently}")
+}
+
+fn projects_block(projects: &Yaml) -> String {
+    let projects = projects.as_vec().unwrap();
+    let mut list: Vec<String> = vec![];
+
+    for (i, p) in projects.iter().enumerate() {
+        let description = p["description"].as_str().unwrap();
+        let term = p["term"].as_str().unwrap();
+        let members = p["members"].as_str().unwrap();
+        let role = p["role"].as_str().unwrap();
+        let assigned_for = p["assigned_for"].as_str().unwrap();
+        let achievement = p["achievement"].as_str().unwrap();
+        let phase = p["phase"].as_str().unwrap();
+        let technology = p["technology"].as_str().unwrap();
+        let others = p["others"].as_str().unwrap();
+
+        let mut owned = format!("| {} | {description} | {term} | {members} | {role} ", i + 1).to_owned();
+        owned.push_str(&format!("| {assigned_for} | {achievement} | {phase} | {technology} | {others} |"));
+        list.push(owned);
+    }
+
+    let projects_str = list.join("\n");
+
+    return format!("
+| No. | 概要 | 期間 | 人数 | ロール | 担当業務 | 習得スキル・実績 | 参画フェーズ | 言語・OS・DB | その他ツール等 |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+{projects_str}")
 }
